a9cdcbcb29259285d955bd5d1d69ffd82a1c1a14
Alex Fraga Belo <alexbelo2.afb@gmail.com>
2018-05-14 08:52:51 -0300

    -Add Resouces

---
+++ GerenciarEquipe.Painel/Scripts/editor.js
@@ -0,0 +1,124 @@
+﻿// Tiny MCE
+tinymce.init({
+    mode: "textareas",
+    editor_selector: "tinymce_basic",
+    editor_deselector: "Notinymce_basic",
+    toolbar: 'fontsizeselect fontselect | bold italic underline | alignleft aligncenter alignright alignjustify table | bullist numlist outdent indent | link image | charmap insertdatetime code',
+    language: 'pt',
+    plugins: [
+    'advlist autolink lists link image charmap print preview anchor',
+    'searchreplace visualblocks code fullscreen',
+    'insertdatetime media table contextmenu paste imagetools autoresize'
+    ],
+    autoresize_min_height: 150,
+    autoresize_max_height: 450,
+    menubar: false,
+    imagetools_cors_hosts: ['www.tinymce.com', 'codepen.io'],
+    content_css: "~/Scripts/tinymce/css/content.css",
+    script_url: "~/Scripts/tinymce/tiny_mce.js",
+       // Drop lists for link/image/media/template dialogs
+    template_external_list_url: "lists/template_list.js",
+    external_link_list_url: "lists/link_list.js",
+    external_image_list_url: "lists/image_list.js",
+    media_external_list_url: "lists/media_list.js",
+    statusbar: false,
+    image_title: true,
+    // enable automatic uploads of images represented by blob or data URIs
+    automatic_uploads: true,
+    //// URL of our upload handler (for more details check: https://www.tinymce.com/docs/configure/file-image-upload/#images_upload_url)
+    //images_upload_url: 'postAcceptor.php',
+    // here we add custom filepicker only to Image dialog
+    file_picker_types: 'image',
+    // and here's our custom image picker
+    file_picker_callback: function (cb, value, meta) {
+        var input = document.createElement('input');
+        input.setAttribute('type', 'file');
+        input.setAttribute('accept', 'image/*');
+
+        // Note: In modern browsers input[type="file"] is functional without 
+        // even adding it to the DOM, but that might not be the case in some older
+        // or quirky browsers like IE, so you might want to add it to the DOM
+        // just in case, and visually hide it. And do not forget do remove it
+        // once you do not need it anymore.
+
+        input.onchange = function () {
+            var file = this.files[0];
+
+            // Note: Now we need to register the blob in TinyMCEs image blob
+            // registry. In the next release this part hopefully won't be
+            // necessary, as we are looking to handle it internally.
+            var id = 'blobid' + (new Date()).getTime();
+            var blobCache = tinymce.activeEditor.editorUpload.blobCache;
+            var blobInfo = blobCache.create(id, file);
+            blobCache.add(blobInfo);
+
+            // call the callback and populate the Title field with the file name
+            cb(blobInfo.blobUri(), { title: file.name });
+        };
+
+        input.click();
+    }
+});
+
+// Tiny MCE
+tinymce.init({
+    mode: "textareas",
+    theme: "advanced",
+    editor_selector: "tinymce_full",
+    editor_deselector: "Notinymce_full",
+    toolbar1: "insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image",
+    toolbar2: "print preview media | forecolor backcolor emoticons",
+    language: 'pt',
+    plugins: [
+    'advlist autolink lists link image charmap print preview anchor',
+    'searchreplace visualblocks code fullscreen',
+    'insertdatetime media table contextmenu paste code imagetools autoresize'
+    ],
+    autoresize_min_height: 150,
+    autoresize_max_height: 450,
+    menubar: false,
+    imagetools_cors_hosts: ['www.tinymce.com', 'codepen.io'],
+    content_css: "~/Scripts/tinymce/css/content.css",
+    // Drop lists for link/image/media/template dialogs
+    template_external_list_url: "lists/template_list.js",
+    external_link_list_url: "lists/link_list.js",
+    external_image_list_url: "lists/image_list.js",
+    media_external_list_url: "lists/media_list.js",
+    statusbar: false,
+    image_title: true,
+    // enable automatic uploads of images represented by blob or data URIs
+    automatic_uploads: true,
+    //// URL of our upload handler (for more details check: https://www.tinymce.com/docs/configure/file-image-upload/#images_upload_url)
+    //images_upload_url: 'postAcceptor.php',
+    // here we add custom filepicker only to Image dialog
+    file_picker_types: 'image',
+    // and here's our custom image picker
+    file_picker_callback: function (cb, value, meta) {
+        var input = document.createElement('input');
+        input.setAttribute('type', 'file');
+        input.setAttribute('accept', 'image/*');
+
+        // Note: In modern browsers input[type="file"] is functional without 
+        // even adding it to the DOM, but that might not be the case in some older
+        // or quirky browsers like IE, so you might want to add it to the DOM
+        // just in case, and visually hide it. And do not forget do remove it
+        // once you do not need it anymore.
+
+        input.onchange = function () {
+            var file = this.files[0];
+
+            // Note: Now we need to register the blob in TinyMCEs image blob
+            // registry. In the next release this part hopefully won't be
+            // necessary, as we are looking to handle it internally.
+            var id = 'blobid' + (new Date()).getTime();
+            var blobCache = tinymce.activeEditor.editorUpload.blobCache;
+            var blobInfo = blobCache.create(id, file);
+            blobCache.add(blobInfo);
+
+            // call the callback and populate the Title field with the file name
+            cb(blobInfo.blobUri(), { title: file.name });
+        };
+
+        input.click();
+    }
+});
